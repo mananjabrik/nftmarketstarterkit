c721d7c4afc8fa6d45efaf0c854eb5c9469add7b
mananjabrik <manan.jabrik@gmail.com>
2022-01-14 19:45:33 +0700

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,48 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+const projectId = 'infuraprojectid';
+const fs = require('fs');
+const keyData = fs.readFileSync('.secret', {
+  encoding: 'utf8',
+  flag: 'r',
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 1337, // config standart localhost
+    },
+    //bsc testnet
+    testnet: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [keyData],
+    },
+    //eth testnet ropsten
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${projectId}`,
+      accounts: [keyData],
+    },
+    //eth testnet mumbai
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [keyData],
+    },
+    //mainnet eth
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${projectId}`,
+      accounts: [keyData],
+    },
+  },
+  solidity: {
+    version: '0.8.4',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
